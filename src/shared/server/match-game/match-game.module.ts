d8d500e6b11498f920311427bc90bcef28790f74
Devo Astroman <devo.astroman@gmail.com>
2024-08-23 13:44:23 +0200

    working on weld

--- src/shared/server/match-game/match-game.module.ts
+++ src/shared/server/match-game/match-game.module.ts
@@ -1,16 +1,20 @@
-import { MatchPlayerService } from "./services/match-players-service.module";
+import { AttachementWelder } from "shared/utils/attachment-welder.module";
+import { MatchPlayerService, PLAYER_CHARACTER } from "./services/match-players-service.module";
+import { BirdStatueWeldManager } from "./services/bird-statue-weld-manager.module";
 
 export class MatchGame {
 	private matchPlayerService: MatchPlayerService;
+	private birdStatueWeldManager: BirdStatueWeldManager;
 
 	constructor(playersInMatch: Model[]) {
 		this.matchPlayerService = new MatchPlayerService(playersInMatch);
+		this.birdStatueWeldManager = new BirdStatueWeldManager(playersInMatch);
 	}
 
 	init() {
 		this.matchPlayerService.enablePlayersJump();
+		this.birdStatueWeldManager.active();
 		/* Should start logic the the match */
-		print("Start match logic");
 	}
 
 	end() {}
