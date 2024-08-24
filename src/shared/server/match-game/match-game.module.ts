aaadb60d745e1db41766b447de0475ceef088caa
Devo Astroman <devo.astroman@gmail.com>
2024-08-24 08:33:35 +0200

    remove useless files

--- src/shared/server/match-game/match-game.module.ts
+++ src/shared/server/match-game/match-game.module.ts
@@ -1,18 +1,15 @@
 import { AttachementWelder } from "shared/utils/attachment-welder.module";
 import { MatchPlayerService, PLAYER_CHARACTER } from "./services/match-players-service.module";
 import { BirdStatueWeldManager } from "./services/bird-statue-weld-manager.module";
-import { BirdStatuesDesk } from "./bird-statues-desk/bird-statues-desk.module";
 import { SLOTS_DESK_INITIAL_VALUES } from "shared/constants.module";
 
 export class MatchGame {
 	private matchPlayerService: MatchPlayerService;
 	private birdStatueWeldManager: BirdStatueWeldManager;
-	private birdStatuesDesk: BirdStatuesDesk;
 
 	constructor(playersInMatch: Model[]) {
 		this.matchPlayerService = new MatchPlayerService(playersInMatch);
 		this.birdStatueWeldManager = new BirdStatueWeldManager(playersInMatch);
-		this.birdStatuesDesk = new BirdStatuesDesk(SLOTS_DESK_INITIAL_VALUES);
 	}
 
 	init() {
